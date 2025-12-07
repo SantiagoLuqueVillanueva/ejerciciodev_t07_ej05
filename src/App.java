da470379134bedb46a5fbbf88c0cee8e3a0595f3
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-12-07 09:36:53 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] num = new int[8];
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        System.out.println("Vaya introduciendo números enteros y pulsando INTRO:");
+
+        for (int i = 0; i < num.length - 1; i++) {
+            num[i] = Integer.parseInt(System.console().readLine());
+            min = num[i] < min ? num[i]:min;
+            max = num[i] > max ? num[i]:max;
+        }
+
+        System.out.println();
+
+        for (int numero : num) {
+            System.out.print(numero);
+            System.out.print(numero == max ? " maximo" : "");
+            System.out.print(numero == min ? " minimo" : "");
+            System.out.println();
+        }
     }
 }
